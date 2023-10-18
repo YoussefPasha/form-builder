70aee55b82e1494fe0db3cef38e6a6d7a3a11502
youssefpasha <youssefbashafcis@gmail.com>
2023-10-18 13:05:26 +0300

    draging above and under fields

--- app/(dashboard)/builder/[id]/_components/designer-element-wrapper.tsx
+++ app/(dashboard)/builder/[id]/_components/designer-element-wrapper.tsx
@@ -2,15 +2,52 @@
 
 import React from "react";
 import { FormElementInstance, FormElements } from "./form-elements";
+import { useDroppable } from "@dnd-kit/core";
 
 const DesignerElementWrapper = ({
   element,
 }: {
   element: FormElementInstance;
 }) => {
+  const topHalf = useDroppable({
+    id: element.id + "-top",
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isTopHalfDesignElement: true,
+    },
+  });
+
+  const bottomHalf = useDroppable({
+    id: element.id + "-bottom",
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isBottomHalfDesignElement: true,
+    },
+  });
+
   const DesignerElement = FormElements[element.type].designerComponent;
 
-  return <DesignerElement />;
+  return (
+    <div className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset ">
+      <div
+        ref={topHalf.setNodeRef}
+        className="absolute  w-full h-1/2 rounded-t-md"
+      ></div>
+      <div
+        ref={bottomHalf.setNodeRef}
+        className="absolute w-full h-1/2 bottom-0 rounded-b-md"
+      ></div>
+      <div
+        className="flex -full h-[120px] 
+      items-center rounded-md bg-accent/40 
+      px-4 py-2 pointer-events-none "
+      >
+        <DesignerElement elementInstance={element} />
+      </div>
+    </div>
+  );
 };
 
 export default DesignerElementWrapper;
--- app/(dashboard)/builder/[id]/_components/fields/text-fields.tsx
+++ app/(dashboard)/builder/[id]/_components/fields/text-fields.tsx
@@ -1,27 +1,62 @@
 "use client";
 
 import { MdTextFields } from "react-icons/md";
-import { ElementsType, FormElement } from "../form-elements";
+import {
+  ElementsType,
+  FormElement,
+  FormElementInstance,
+} from "../form-elements";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const type: ElementsType = "TextField";
+
+const extraAttributes = {
+  label: "Text field",
+  helperText: "Helper text",
+  required: false,
+  placeHolder: "Value here...",
+};
 
 export const TextFieldFormElement: FormElement = {
   type,
   construct: (id: string) => ({
     id,
     type,
-    extraAttributes: {
-      label: "Text field",
-      helperText: "Helper text",
-      required: false,
-      placeHolder: "Value here...",
-    },
+    extraAttributes,
   }),
   designerBtnElement: {
     icon: MdTextFields,
     label: "Text Field",
   },
-  designerComponent: () => <div>Designer component</div>,
+  designerComponent: DesignerComponent,
   formComponent: () => <div>Form component</div>,
   propertiesComponent: () => <div>Properties component</div>,
 };
+
+type CustomInstance = FormElementInstance & {
+  extraAttributes: typeof extraAttributes;
+};
+
+function DesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+
+  const { helperText, label, placeHolder, required } = element.extraAttributes;
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Input readOnly disabled placeholder={placeHolder} />
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
