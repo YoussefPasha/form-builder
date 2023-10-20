643b627667c0b9d04fcaa8864f9a743975a0d64b
youssefpasha <youssefbashafcis@gmail.com>
2023-10-20 09:23:42 +0300

    publish page

--- actions/form.ts
+++ actions/form.ts
@@ -113,4 +113,19 @@ export async function UpdateFormContent(id: number, jsonContent: string) {
   });
 }
 
-export async function PublishForm(id: number) {}
+export async function PublishForm(id: number) {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.update({
+    where: {
+      userId: user.id,
+      id,
+    },
+    data: {
+      published: true,
+    },
+  });
+}
